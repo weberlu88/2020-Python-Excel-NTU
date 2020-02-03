d3c5a4c4a3bd3b01f6a4956f3fcc014bcbc533be
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2020-02-03 21:06:32 +0800

    Update 01-stock-daily-return.py

--- 01-stock-daily-return.py
+++ 01-stock-daily-return.py
@@ -5,12 +5,19 @@ import xlwings as xw
 wb = xw.Book(r"stock_price_data.xlsx")
 tsmc_sheet = wb.sheets["2330"]
 
+def calc_daily_return(today_price, yesterday_price):
+    daily_return = (today_price - yesterday_price)*100 / yesterday_price
+    return daily_return
+
+def calc_moving-avg():
+    return
+
 # 算出所有的報酬率
 for i in range(3, 97): # 3~96
     # Get current price and previous price
     today_price = tsmc_sheet.range(f"B{i}").value
     yesterday_price = tsmc_sheet.range(f"B{i-1}").value
     # Calc return value and write in excel
-    daily_return = (today_price - yesterday_price)*100 / yesterday_price
+    daily_return = calc_daily_return(today_price, yesterday_price)
     tsmc_sheet.range(f"C{i}").value = daily_return
     print(str(i) + ": " + str(daily_return))
